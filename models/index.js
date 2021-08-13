2b3920a4a7bd7ceda07f57c1f05ed78f18e8cb31
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-13 11:37:47 -0700

    added model relationships to models/index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,40 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// User has many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+// User has many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+// Post belongs to a User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Comment belongs to a Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+// Comment belongs to a User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = {User, Post, Comment}
 // User model
 // - ID
 // - Username

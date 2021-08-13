0d1c3cb21cc88b7ec6d7f0f4add9e87cd201ebb1
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-13 15:11:45 -0700

    added blank view files

--- models/index.js
+++ models/index.js
@@ -36,30 +36,3 @@ Comment.belongsTo(User, {
 });
 
 module.exports = {User, Post, Comment}
-
-// User model
-// - ID
-// - Username
-// - Password
-// 
-
-// Post Model
-// - ID 
-// - title
-// - contents
-// - post creator foreign key to (user_id: one user have many posts)
-// - Date created
-
-// Comment Model
-// - ID
-// - content
-// - Post ID foreign key to post.id one post have many comments
-// - comment creator (user_id: one user has many posts)
-// - Date created
-
-// Views
-// - Homepage
-// - single blog post page
-// - dashboard page
-// - add post page
-// - edit post page

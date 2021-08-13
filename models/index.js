768f104d01798b745048579b0c00d402e12d5d86
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-13 10:03:41 -0700

    added model files, set up User model

---
+++ models/index.js
@@ -0,0 +1,26 @@
+// User model
+// - ID
+// - Username
+// - Password
+// 
+
+// Post Model
+// - ID 
+// - title
+// - contents
+// - post creator foreign key to (user_id: one user have many posts)
+// - Date created
+
+// Comment Model
+// - ID
+// - content
+// - Post ID foreign key to post.id one post have many comments
+// - comment creator (user_id: one user has many posts)
+// - Date created
+
+// Views
+// - Homepage
+// - single blog post page
+// - dashboard page
+// - add post page
+// - edit post page

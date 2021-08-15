d63e17880ece366e83c91b2a5d93343c8dddbd79
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-15 15:42:58 -0700

    trouble shooting session secret

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+app.use(session({ secret: process.env.SECRET }));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);

550f3db202d2f192421547bc329c1dbaeaa402ff
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-14 20:07:22 -0700

    changed the get post by id route to also find all the comments associated with it then pass both those objects to the blogPage view

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,14 +3,23 @@ const { Post, User, Comment } = require('../../models');
 const router = require('express').Router();
 // const withAuth = require('../../utils/auth');
 
+// get a post and its comments
 router.get("/:id", async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
-            include: [{ model: User, attributes: ['username'] }, Comment]
+            include: [{ model: User, attributes: ['username'] }]
         });
         const post = postData.get({ plain: true });
         console.log(post);
-        res.render('blogPage', { post });
+
+        const commentData = await Comment.findAll({
+            where: { post_id: post.id },
+            include: [{ model: User, attributes: ['username'] }]
+        });
+        const comments = commentData.map((comment) => comment.get({plain: true}));
+
+        console.log(comments);
+        res.render('blogPage', { post, comments });
 
     } catch (err) {
         res.status(500).json(err);
